40895211fd29d22625fbaa04320af6545e7942be
glandty <glandtya@163.com>
2020-07-29 16:10:44 +0800

    Initial commit

---
+++ src/MyQueue.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> A = new Stack<>();
+    private Stack<Integer> B = new Stack<>();
+
+    public void push(int x) {
+        while(!B.isEmpty()) {
+            int tmp = B.pop();
+            A.push(tmp);
+        }
+        A.push(x);
+    }
+
+    public Integer pop() {
+        if(empty()) {
+            return null;
+        }
+        while(!A.isEmpty()) {
+            int tmp = A.pop();
+            B.push(tmp);
+        }
+        return B.pop();
+    }
+
+    public Integer peek() {
+        if(empty()) {
+            return null;
+        }
+        while(!A.isEmpty()) {
+            int tmp = A.pop();
+            B.push(tmp);
+        }
+        return B.peek();
+    }
+
+    public boolean empty() {
+        return A.isEmpty() && B.isEmpty();
+    }
+}
